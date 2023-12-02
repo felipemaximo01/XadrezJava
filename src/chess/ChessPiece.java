ef08ed8dc3c3f4e5249f35393199d268bbdcb870
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-02 19:53:59 -0300

    contador de movimentos da peça

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,6 +8,8 @@ public abstract class ChessPiece extends Piece {
 
     private Color color;
 
+    private int moveCount;
+
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
@@ -17,13 +19,25 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
-    protected boolean isThereOpponentPiece(Position position){
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
 
-    public ChessPosition getChessPosition(){
+    public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
-  
+
 }
